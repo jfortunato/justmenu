f5e077d55ca188f308cbe6340f334ad968fab39c
Justin Fortunato <jfortunato25@gmail.com>
2014-02-15 15:19:15 -0500

    setup grunt

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	uglify: {
+		dev: {
+			options: {
+				sourceMap: 'public/assets/compiled/<%= pkg.name %>.min.map.js',
+				sourceMappingURL: '<%= pkg.name %>.min.map.js',
+			},
+			files: {
+				'public/assets/compiled/<%= pkg.name %>.min.js': ['frontend/js/helpers.js', 'frontend/js/optionbox.js', 'frontend/js/storage.js', 'frontend/js/cart.js', 'frontend/js/template.js', 'frontend/js/view.js', 'frontend/js/controller.js', 'frontend/js/app.js']
+			}
+		}
+	},
+	sass: {
+		dev: {
+			options: {
+				sourcemap: true,
+				style: 'compressed',
+			},
+			files: {
+				'public/assets/compiled/justmenu.min.css': 'frontend/sass/main.scss'
+			}
+		}
+	},
+	jshint: {
+		beforeconcat: ['frontend/js/**/*.js'],
+	},
+	watch: {
+		options: {
+			livereload: true,
+		},
+		js: {
+			files: ['frontend/js/**/*.js'],
+			tasks: ['jshint', 'uglify:dev']
+		},
+		sass: {
+			files: ['frontend/sass/**/*.scss'],
+			tasks: ['sass:dev']
+		}
+	}
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify:dev', 'sass:dev']);
+
+};
--- public/index.php
+++ public/index.php
@@ -18,13 +18,6 @@ $view = $cart->render() . $view;
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <!--<link rel="stylesheet" href="assets/lib/css/bootstrap.min.css">-->
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
     </head>
     <body>
 
@@ -36,18 +29,6 @@ $view = $cart->render() . $view;
             </div>
         </div>
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <!--<script src="assets/lib/js/jquery.js"></script>-->
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <!--<script src="assets/lib/js/bootstrap.min.js"></script>-->
-
-        <script src="assets/js/helpers.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/optionbox.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/storage.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/cart.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/template.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/view.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/controller.js" type="text/javascript" charset="utf-8"></script>
-        <script src="assets/js/app.js" type="text/javascript" charset="utf-8"></script>
+        <script src="assets/compiled/justmenu.min.js" type="text/javascript" charset="utf-8"></script>
     </body>
 </html>
